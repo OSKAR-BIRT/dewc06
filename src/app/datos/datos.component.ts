58d45f8719e0bb19fa24566c04946b095fd2c9bc
Oskar <oprieto@birt.eus>
2026-01-03 01:07:38 +0100

    Commit inicial

---
+++ src/app/datos/datos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datos',
+  standalone: false,
+  templateUrl: './datos.component.html',
+  styleUrl: './datos.component.css'
+})
+export class DatosComponent {
+
+}
